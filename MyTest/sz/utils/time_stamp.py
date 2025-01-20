27594056232ec517ab4e5c9360cf7418859b7a09
yangjing <yangjing@yunzhangfang.com>
2025-01-20 20:10:06 +0800

    update time stamp

--- MyTest/sz/utils/time_stamp.py
+++ MyTest/sz/utils/time_stamp.py
@@ -1,11 +1,15 @@
-import time
+from datetime import datetime
 
 
 def return_strftime(time_stamp):
-    time_array = time.localtime(int(time_stamp))
-    return time.strftime("%Y-%m-%d %H:%M:%S", time_array)
+    if len(str(time_stamp)) >= 10:
+        date_array = datetime.utcfromtimestamp(int(str(time_stamp)[:10]))
+    else:
+        date_array = datetime.utcfromtimestamp(int(time_stamp))
+    res = date_array.strftime("%Y-%m-%d %H:%M:%S")
+    return res
 
 
 if __name__ == '__main__':
-    a = 1730671263723
+    a = 1737820799000
     print(return_strftime(a))
